bb9d5b0546eb1a8b9428966896ea54a33012a15a
SJAsus <sjasus@shalabh.us>
2017-11-15 20:29:59 -0500

    Updated to Apache License Version 2.0

--- src/main/java/us/shalabh/alp/service/IDemoS3EventService.java
+++ src/main/java/us/shalabh/alp/service/IDemoS3EventService.java
@@ -1,14 +1,17 @@
 /** 
- * Copyright 2017. All Rights Reserved.
+ * Copyright 2017 shalabh.us
  * 
- * NOTICE:  All information contained herein is, and remains
- * the property of shalabh.us. The intellectual and technical 
- * concepts contained herein are proprietary to shalabh.us 
- * and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from shalabh.us
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package us.shalabh.alp.service;
 
